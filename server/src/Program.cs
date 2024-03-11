bd4ebbfd717db6c98988d3f7260320bc8fb67beb
= <potapovmichail@gmail.com>
2024-03-11 21:55:48 +0200

    server

---
+++ server/src/Program.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.MapHub<ChatHub>("chat");
+app.Run();
